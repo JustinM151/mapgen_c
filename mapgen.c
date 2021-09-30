e84bbb81ad8c3c58dd4db5cd0391eb06b2260983
Justin Morris <justin@fuzioncore.com>
2021-09-29 19:35:27 -0700

    Add abbility to override defaults with args

--- mapgen.c
+++ mapgen.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("Welcome to MapGen!");
+int main(int argc, char *argv[]) {
+  char* filename = "map.bmp";
+  int width  = 1024;
+  int height = 1024;
+
+  if (argc >= 2) {
+    filename = argv[1];
+  }
+
+  if (argc >= 3) {
+    width = atoi(argv[2]);
+  }
+
+  if (argc >= 4) {
+    height = atoi(argv[3]);
+  }
+
+  printf("Welcome to MapGen!\n");
+  printf("Writing to file %s\n", filename);
+  printf("Map Resolution [%d x %d]\n", width,height);
+
 
   return 0;
 }
